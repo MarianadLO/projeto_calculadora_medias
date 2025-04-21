70cf87b599e4f33e54f77506a10e5e2f4f934ba1
lima79197@gmail.com <lima79197@gmail.com>
2025-04-21 18:07:42 -0300

    fixing the scale

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function adicionaLinha(){
         linha += `<td>${inputNotaDaAtividade.value}</td>`
         linha += `<td>${inputNotaDaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`
         linha += `</tr>`
-        
+
         linhas += linha
     }
     
@@ -48,7 +48,7 @@ function atualizaTabela(){
 
 function atualizaMediaFinal(){
     const mediaFinal = calculaMediaFinal()
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
 }
 
